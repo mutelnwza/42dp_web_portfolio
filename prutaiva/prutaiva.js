8673d8e3c905d827e4b1881c61f1c3bebe2137a5
mutelnwza <mutemallow@gmail.com>
2024-03-29 01:52:53 +0700

    cert left

--- prutaiva/prutaiva.js
+++ prutaiva/prutaiva.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "nav") {
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "nav";
+      x.className = "topnav";
     }
   }
